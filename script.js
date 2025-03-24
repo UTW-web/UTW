ff8c299fd373050d00d67e3cc6d6df489447a988
UTW-web <jonas.abelnx@gmail.com>
2025-03-24 19:30:48 +0100

    popravek <a> link in smooth scrolling

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+ // Smooth scroll for anchor links
+ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 // Reveal animations on scroll
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
